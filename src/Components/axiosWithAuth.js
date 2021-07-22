76f981f222b5ebd7cfd314f5287312ef8eed720a
JohnGrabowskiIII <johngrabowski97@gmail.com>
2021-07-22 00:08:12 -0400

    Created Components folder, created axiosWithAuth component

---
+++ src/Components/axiosWithAuth.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import axios from 'axios';
+
+const axiosWithAuth = () => {
+
+    const token = window.localStorage.getItem('token');
+
+    return axios.create({
+        headers: {
+            Authorization: token
+        },
+    })
+
+}
+
+export default axiosWithAuth;
