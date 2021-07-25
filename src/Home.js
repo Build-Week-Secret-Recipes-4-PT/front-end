20a80f51b9887bca2d72f248722202c99590d21d
Owen  Damron <damronowen@yahoo.com>
2021-07-25 18:29:59 -0500

    Added nav and name

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,30 @@
 import React from "react";
 import RecipeForm from "./RecipeForm";
-import {Switch, Route} from "react-router-dom"
+import {Switch, Route, Link} from "react-router-dom"
 import './App.css';
+
 
 export default function Home() {
     return (
       <div className='home-page'>
-        <img
-        className='home-page-image' 
-        src=
-        'https://media.istockphoto.com/photos/varied-food-carbohydrates-protein-vegetables-fruits-dairy-legumes-on-picture-id1218254547?b=1&k=6&m=1218254547&s=170667a&w=0&h=EXwwoHJ3wI0H2jDfoFhqOiIo2c4cL0y7R8Gop3iIO30='
-        alt='Veggies and Protiens'
-        />
-      <h1></h1>
-      <h2>Recipes</h2>
-      <Switch>
+        <Switch>
         <Route path='/home'>
-          <RecipeForm />
+          <div className='nav'>            
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Login/Register</Link>
+              </li>
+            </ul>
+          </nav>
+          </div>
+      <h1>{details.name}</h1>
+      <h2>Recipes</h2>
+      <RecipeForm />
         </Route>
       </Switch>
       </div>
     );
   }
+
+  
